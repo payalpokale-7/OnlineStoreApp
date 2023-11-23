593f433aaeaae1bea16b5e0cb8078401c64a0825
payal pokale <78795031+payalpokale-7@users.noreply.github.com>
2023-11-23 13:06:44 +0530

    Create userActions.js

---
+++ userActions.js
@@ -0,0 +1,14 @@
+// userActions.js
+export const registerUser = (userData) => {
+  return {
+    type: 'REGISTER_USER',
+    payload: userData,
+  };
+};
+
+export const loginUser = (userData) => {
+  return {
+    type: 'LOGIN_USER',
+    payload: userData,
+  };
+};
